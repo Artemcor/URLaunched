eceac94a901de1609e7bd7a6227919daa10a5d29
Artem Stozhok <artem.stozhok@vlogbox.com>
2023-04-24 12:44:01 +0300

    Initial Commit

---
+++ URLaunched/URLaunchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLaunchedApp.swift
+//  URLaunched
+//
+//  Created by Artem Stozhok on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct URLaunchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
